269446ca960ce1b09613e5cb066dc30a09f3876e
rgerd <rgerdisch@gmail.com>
2022-08-20 13:46:34 -0700

    Logging face loops

--- src/operations/vertex_measures.rs
+++ src/operations/vertex_measures.rs
@@ -1,5 +1,7 @@
 //! See [Mesh](crate::mesh::Mesh).
 
+use std::collections::HashSet;
+
 use crate::prelude::*;
 
 /// # Vertex measures
@@ -7,10 +9,22 @@ impl Mesh {
     /// Returns the normal of the vertex given as the average of the normals of the neighbouring faces.
     pub fn vertex_normal(&self, vertex_id: VertexID) -> Vec3 {
         let mut normal = Vec3::zero();
+        let mut seen_faces = HashSet::<FaceID>::new();
+        let mut face_count = 0;
         for halfedge_id in self.vertex_halfedge_iter(vertex_id) {
             if let Some(face_id) = self.walker_from_halfedge(halfedge_id).face_id() {
-                normal += self.face_normal(face_id)
+                if !seen_faces.insert(face_id) {
+                    println!("Hit a loop!");
+                    break;
+                }
+                normal += self.face_normal(face_id);
+                face_count += 1;
             }
+        }
+        assert!(face_count > 0);
+        if normal.magnitude2() == 0.0 {
+            println!("Got a zero mag!");
+            normal = Vec3::unit_z();
         }
         normal.normalize()
     }
